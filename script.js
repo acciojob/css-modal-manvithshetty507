b6b3bcb764f619d159ad792ad19bd84311a841d9
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-17 13:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const btn = document.getElementById('openModal');
+
+btn.addEventListener('click',btnClicked)
+
+function btnClicked() {
+	document.body.classList.add('active');
+	document.getElementsByClassName('modal')[0].style.display = 'flex';
+	document.getElementById('close-btn').addEventListener('click',closeModal);
+	btn.disabled = true;
+	function closeModal() {
+		document.getElementsByClassName('modal')[0].style.display = 'none';
+		document.body.classList.remove('active');
+		btn.disabled = false;
+	}
+}
+
